697cf72fb8e61b993d3ae560ca16fe6d39a9fc51
Simon <contact@smn.codes>
2025-01-14 00:52:37 +0100

    Cache size limit

--- galette/cache.py
+++ galette/cache.py
@@ -7,10 +7,21 @@ class PageCacheItem(TypedDict):
     body: str
 
 class PageCache:
+    _maxsize: int
     _store: dict[str, PageCacheItem] = {}
 
-    def set(self, id: str, **item: PageCacheItem):
+    def __init__(self, maxsize: int = 512) -> None:
+        self._maxsize = maxsize
+
+    def set(self, id: str, **item: PageCacheItem) -> None:
+        if len(self._store) > self._maxsize:
+            oldest = min(self._store, key=lambda item: item['timestamp'])
+            self.delete(oldest)
+
         self._store[id] = item
         
     def get(self, id: str) -> PageCacheItem|None:
         return self._store[id] if id in self._store else None
+
+    def delete(self, id: str) -> None:
+        del self._store[id]
